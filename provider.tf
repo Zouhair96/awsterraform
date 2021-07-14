fa58381e3602d3c855b6d3e6c592a8eae3165ba6
Zouhair96 <zouhair.benali96@gmail.com>
2021-07-14 10:36:39 +0100

    initialisation de projet

---
+++ provider.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+
+  profile = "zouhaier"
+  region  = "us-east-1"
+}
+
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+      version = "~> 3.21"
+    }
+  }
+}
